spesmilo/electrum

qt: initial support for bolt12 offers

electrum/gui/qt/main_window.py
@@ -50,7 +50,7 @@ import electrum_ecc as ecc
 import electrum
 from electrum.gui import messages
 from electrum import (keystore, constants, util, bitcoin, commands,
-                      paymentrequest, lnutil)
+                      paymentrequest, lnutil, bolt12)
 from electrum.bitcoin import COIN, is_address, DummyAddress
 from electrum.plugin import run_hook
 from electrum.i18n import _
@@ -1704,7 +1704,13 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger, QtEventListener):
 
     def show_lightning_invoice(self, invoice: Invoice):
         from electrum.util import format_short_id
-        lnaddr = lndecode(invoice.lightning_invoice)
+        if bolt12_invoice_tlv := invoice.bolt12_invoice_tlv():
+            bolt12_inv = bolt12.decode_invoice(bolt12_invoice_tlv)
+            lnaddr = bolt12.to_lnaddr(bolt12_inv)
+        elif invoice.lightning_invoice:
+            lnaddr = lndecode(invoice.lightning_invoice)  # FIXME: assumes BOLT11, should be abstracted by Invoice
+        else:
+            raise Exception()
         d = WindowModalDialog(self, _("Lightning Invoice"))
         vbox = QVBoxLayout(d)
         grid = QGridLayout()

f321x/electrum-fork

electrum/gui/qt/main_window.py
@@ -50,7 +50,7 @@ import electrum_ecc as ecc
 import electrum
 from electrum.gui import messages
 from electrum import (keystore, constants, util, bitcoin, commands,
-                      lnutil)
+                      lnutil, bolt12)
 from electrum.bitcoin import COIN, is_address, DummyAddress
 from electrum.plugin import run_hook
 from electrum.i18n import _
@@ -1678,7 +1678,13 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger, QtEventListener):
 
     def show_lightning_invoice(self, invoice: Invoice):
         from electrum.util import format_short_id
-        lnaddr = decode_bolt11_invoice(invoice.lightning_invoice)
+        if bolt12_invoice_tlv := invoice.bolt12_invoice_tlv():
+            bolt12_inv = bolt12.BOLT12Invoice.decode(bolt12_invoice_tlv)
+            lnaddr = bolt12.to_lnaddr(bolt12_inv)
+        elif invoice.lightning_invoice:
+            lnaddr = decode_bolt11_invoice(invoice.lightning_invoice)  # FIXME: assumes BOLT11, should be abstracted by Invoice
+        else:
+            raise Exception()
         d = WindowModalDialog(self, _("Lightning Invoice"))
         vbox = QVBoxLayout(d)
         grid = QGridLayout()