edcd8da00a4461a588f1182481112e58abfca76a
f321x <f@f321x.com>
2026-09-09 14:51:31 +0200

    qt: use Qt Multimedia and zxing-cpp for desktop QR scanning
    Also use zxing-cpp on desktop (instead of zbar), so we have only
    one type of scanning library and can also benefit from the better
    performance of zxing on desktop.
    The tradeoff is that the linux scanning path now needs QtMultimedia
    too instead of feeding the camera stream directly into the scanning
    library. Windows and MacOS were already using QtMultimedia based
    camera access before.
    zxing-cpp also needs a C++20 compatible compiler.

--- electrum/qrreader/__init__.py
+++ electrum/qrreader/__init__.py
@@ -25,7 +25,7 @@
 #
 # A module, that, given an image (buffer), finds and decodes a QR code in it.
 
-import os
+from typing import Mapping, Optional
 
 from ..logging import get_logger
 
@@ -36,26 +36,21 @@ _logger = get_logger(__name__)
 
 
 class MissingQrDetectionLib(RuntimeError):
-    ''' Raised if we can't find zbar or whatever other platform lib
-    we require to detect QR in image frames. '''
+    ''' Raised if the library required to detect QR codes is unavailable. '''
 
 
 def get_qr_reader() -> AbstractQrCodeReader:
     """
-    Get the Qr code reader for the current platform.
+    Get the QR code reader.
     Might raise exception: MissingQrDetectionLib.
     """
-    excs = []
     try:
-        if 'ANDROID_DATA' in os.environ:
-            from .zxing import ZXingQrCodeReader
-            return ZXingQrCodeReader()
-        from .zbar import ZbarQrCodeReader
-        return ZbarQrCodeReader()
+        from .zxing import ZXingQrCodeReader
+        return ZXingQrCodeReader()
         """
         # DEBUG CODE BELOW
         # If you want to test this code on a platform that doesn't yet work or have
-        # zbar, use the below...
+        # zxing-cpp, use the below...
         class Fake(AbstractQrCodeReader):
             def read_qr_code(self, buffer, buffer_size, dummy, width, height, frame_id = -1):
                 ''' fake noop to test '''
@@ -64,9 +59,14 @@ def get_qr_reader() -> AbstractQrCodeReader:
         """
     except MissingLib as e:
         _logger.exception("")
-        excs.append(e)
+        raise MissingQrDetectionLib(f"The QR detection library is not available.\n{e}") from e
 
-    raise MissingQrDetectionLib(f"The platform QR detection library is not available.\nerrors: {excs!r}")
+
+def version_info() -> Mapping[str, Optional[str]]:
+    from .zxing import LIBZXING
+    return {
+        "libZXing.path": LIBZXING._name if LIBZXING else None,
+    }
 
 
 # --- Internals below (not part of external API)
